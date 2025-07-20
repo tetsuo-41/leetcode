661e871910cf077254ef9b4d95436f27984d9501
tetsuo-41 <itsukiyama0918@gmail.com>
2025-07-21 03:34:46 +0900

    leetcode

---
+++ blind75/easy/0141_linked_list_style/0141_linked_list_style.py
@@ -0,0 +1,28 @@
+# LeetCode 141. Linked List Style
+# Difficulty: Medium
+# Runtime: 37ms, Beats 57.74%/ Memory: 19.46MB, Beats 85.30%
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True # Cycle detected
+
+        return False # reached end, no cycle
