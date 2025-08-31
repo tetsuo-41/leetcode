c67558ad5a39711af3a639ea6b4c9cb20f19318f
tetsuo-41 <itsukiyama0918@gmail.com>
2025-09-01 07:52:02 +0900

    leetcode

---
+++ blind75/medium/0152_maximum_product_subbaray/0152_maximum_product_subbaray.py
@@ -0,0 +1,26 @@
+# LeetCode 152. Maximum Product Subarray
+# Difficulty: Hard
+# Runtime: 10ms, Beats 61.45%/ Memory: 13.15MB, Beats 53.16%
+
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_prod = nums[0]
+        min_prod = nums[0]
+        ans = nums[0]
+        
+        for i in range(1, len(nums)):
+            num = nums[i]
+            # 負数のときに swap するのがポイント
+            if num < 0:
+                max_prod, min_prod = min_prod, max_prod
+            
+            max_prod = max(num, num * max_prod)
+            min_prod = min(num, num * min_prod)
+            
+            ans = max(ans, max_prod)
+        
+        return ans
