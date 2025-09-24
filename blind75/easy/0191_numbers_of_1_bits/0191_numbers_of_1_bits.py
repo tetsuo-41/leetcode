ca335855c0bdce2ada00923dc9abab2710321c88
tetsuo-41 <itsukiyama0918@gmail.com>
2025-09-24 10:03:55 +0900

    leetcode

---
+++ blind75/easy/0191_numbers_of_1_bits/0191_numbers_of_1_bits.py
@@ -0,0 +1,19 @@
+# LeetCode 62. Unique Paths
+# Difficulty: Easy
+# Runtime: 0ms, Beats 100.00%/ Memory: 12.52MB, Beats 9.27%
+
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # 方法1: 組み込み関数利用
+        return bin(n).count('1')
+
+        # 方法2: ビット演算
+        # count = 0
+        # while n:
+        #     n &= n - 1
+        #     count += 1
+        # return count
